b8a8a458cdc283c220723f9c8d9784fa8f6f0ef0
freshmind2023 <jishujuan@hotmail.com>
2024-12-08 04:07:31 -0500

    Add app3, display image

--- Flask-Project/static/js/forms.js
+++ Flask-Project/static/js/forms.js
@@ -1,6 +1,5 @@
 // Handler for Form 1
 async function handleName(event) {
-    console.log("here")
     event.preventDefault();
     console.log(event.target.type)
     const name = event.target.parentElement.querySelector('input[type="text"]').value;
@@ -16,7 +15,6 @@ async function handleName(event) {
 
 // Handler for Form 2
 async function handleLanguage(event) {
-    console.log("here1")
     event.preventDefault();
     const selectedLanguage = event.target.querySelector('input[name="language"]:checked').value;
     console.log(selectedLanguage)
@@ -29,9 +27,34 @@ async function handleLanguage(event) {
     document.getElementById('responseMessage2').textContent = result.message;
 }
 
+// Update displayed value dynamically
+function updateValue(spanId, value) {
+    document.getElementById(spanId).textContent = value;
+}
+// Submit values to the backend
+async function processValues() {
+    const slider1Value = document.getElementById('slider1').value;
+    const slider2Value = document.getElementById('slider2').value;
+    const response = await fetch('/process', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ value1: slider1Value, value2: slider2Value })
+    });
+    const result = await response.text();
+    document.getElementById('response').textContent = `Result: ${result}`;
+}
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('sendButton').addEventListener('click', handleName);
-    document.getElementById('surveyForm').addEventListener('submit', handleLanguage);
+    const pageName = document.body.getAttribute('data-page-name');
+    console.log("name "+ pageName);
+    if (pageName == 'test1.html') {
+        document.getElementById('sendButton').addEventListener('click', handleName);
+        document.getElementById('surveyForm').addEventListener('submit', handleLanguage);
+    } else if (pageName == 'test2.html') {
+        document.getElementById('slider1').addEventListener('value1', updateValue);
+        document.getElementById('slider2').addEventListener('value2', updateValue);
+        document.getElementById('submitValues').addEventListener('click', processValues);
+    }
 });
